a00e4513b4408d7d009f032a9cdb5ed8081bd4cb
Valentin Radulescu <radulescu.valentin@gmail.com>
2019-02-26 16:01:17 +0200

    Visit SO every 23 hours

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const {email, password} = require("./config.json");
+const { email, password } = require("./config.json");
 const Browser = require("zombie");
+
+const visitInterval = 23 * 60 * 60 * 1000;
 
 // Stop all JS requests, easier than polyfilling zombie/jsdom
 Browser.runScripts = false;
-const browser = new Browser();
 
-browser.visit("https://stackoverflow.com/users/login", () => {
-  Promise.all([
-    browser.fill("email", email),
-    browser.fill("password", password)
-  ])
-    .then(browser.pressButton("Log in"))
-    .then(() => {
-      console.log("OK", browser.location.href);
-    })
-    .catch(err => console.log(err));
-});
+function visit() {
+  const browser = new Browser();
+
+  return browser.visit("https://stackoverflow.com/users/login", () => {
+    Promise.all([
+      browser.fill("email", email),
+      browser.fill("password", password)
+    ])
+      .then(browser.pressButton("Log in"))
+      .then(() => {
+        console.log("OK", browser.location.href);
+      })
+      .catch(err => console.log(err));
+  });
+}
+
+visit();
+setInterval(visit, visitInterval);
